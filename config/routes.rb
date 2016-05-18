dd7102b3fdc25ce904193da17e04ef09a749304b
Marvki <marvkipi@gmail.com>
2016-05-17 21:00:57 -0700

    adds create, update and destroy nested under users resources

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ MarketPlaceApi::Application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/'  do
-    scope module: :v1,
-              constraints: ApiConstraints.new(version: 1, default: true) do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
-      resources :users, :only => [:show, :create, :update, :destroy]
+      resources :users, :only => [:show, :create, :update, :destroy] do
+        # this is the line
+        resources :products, :only => [:create, :update, :destroy]
+      end
       resources :sessions, :only => [:create, :destroy]
       resources :products, :only => [:show, :index]
     end
