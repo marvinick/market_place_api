fd9de60a7f3b2c189467c634c17eb1efae31227b
Marvki <marvkipi@gmail.com>
2016-05-22 09:53:24 -0700

    allows the order the be placed along with product quantity

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -53,7 +53,7 @@ describe Api::V1::OrdersController do
 
       product_1 = FactoryGirl.create :product 
       product_2 = FactoryGirl.create :product
-      order_params = { product_ids: [product_1.id, product_2.id] }
+      order_params = { product_ids_and_quantities: [[product_1.id, 2], [product_2.id, 3]] }
       post :create, user_id: current_user.id, order: order_params 
     end
 
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -10,7 +10,7 @@ describe Order do
   it { should validate_presence_of :user_id }
 
   it { should belong_to :user }
-  it { should have_many(:placements) }
+  it { should have_many(:placements) }  
   it { should have_many(:products).through(:placements) }
 
   describe '#set_total!' do
@@ -23,6 +23,19 @@ describe Order do
 
     it "returns the total amount to pay for the products" do
       expect{@order.set_total!}.to change{@order.total}.from(0).to(185)
+    end
+  end
+
+  describe "#build_placements_with_product_ids_and_quantities" do 
+    before(:each) do 
+      product_1 = FactoryGirl.create :product, price: 100, quantity: 5
+      product_2 = FactoryGirl.create :product, price: 85, quantity: 10
+
+      @product_ids_and_quantities = [[product_1.id, 2], [product_2.id, 3]]
+    end 
+
+    it "builds 2 placements for the order" do 
+      expect{order.build_placements_with_product_ids_and_quantities(@product_ids_and_quantities)}.to change{order.placements.size}.from(0).to(2)
     end
   end
 end
